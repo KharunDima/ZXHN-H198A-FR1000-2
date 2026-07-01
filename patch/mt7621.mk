cc1da9ea70e9847c4250e1d27f2d1674aadea7e8
KeiRun <50458536+KeiRun@users.noreply.github.com>
2026-07-01 21:15:47 +0700

    Update mt7621.mk

--- patch/mt7621.mk
+++ patch/mt7621.mk
@@ -37,6 +37,7 @@ endef
 TARGET_DEVICES += zte_e8820s-nand
 
 define Device/zte_e8820s-spi
+  DEVICE_DTS := mt7621_zte_e8820s-spi
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := E8820S-SPI
   DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb3 luci luci-i18n-base-ru luci-app-ttyd curl jq kmod-nft-tproxy coreutils-base64 bind-dig b4 kmod-nfnetlink kmod-nf-conntrack kmod-nf-conntrack-netlink kmod-ipt-connbytes kmod-ipt-nfqueue kmod-nfnetlink-queue kmod-nft-queue kmod-nf-nat kmod-nft-masq wget-ssl ca-certificates tar
